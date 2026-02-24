7b8953651d0a93b7fa42046d86cc3310475adc57
Lukas Scheller <lukas2121@icloud.com>
2026-02-24 17:23:30 +0100

    Revert "Canonicalize consecutive concats"
    This reverts commit 610e82761129ad5e2476583210c7037b00310542.

--- vhdl-fmt/src/doc_ir/builder.rs
+++ vhdl-fmt/src/doc_ir/builder.rs
@@ -2,14 +2,12 @@ use vhdl_syntax::{syntax::node::SyntaxToken, tokens::Trivia};
 
 use crate::doc_ir::{Doc, DocComment, Docs};
 
-#[derive(Eq, PartialEq, Clone, Copy, Debug)]
 pub enum NodeKind {
     Concat,
     Indent,
     Group,
 }
 
-#[derive(Clone, Debug)]
 pub enum Event {
     Push(Doc),
     Start(NodeKind),
@@ -98,11 +96,6 @@ impl DocBuilder {
                     parents.push(len);
                 }
                 Event::End(node_kind) => {
-                    // Canonicalize: Concat(Concat(X)) -> X
-                    if node_kind == NodeKind::Concat && children.last().is_some_and(|doc| matches!(doc, Doc::Concat(_))) {
-                        parents.pop();
-                        continue;
-                    }
                     let first_child = parents.pop().unwrap();
                     let data = children.drain(first_child..).collect::<Vec<_>>();
                     debug_assert!(
@@ -118,8 +111,6 @@ impl DocBuilder {
             }
         }
         assert!(children.len() == 1);
-        let doc = children.pop().unwrap();
-        println!("{doc:#?}");
-        doc
+        return children.pop().unwrap()
     }
 }
